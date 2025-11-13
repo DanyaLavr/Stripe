1d9d9d36903d7bff892c77a8863cbbae52074d68
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2025-11-13 01:39:13 +0100

    updated hover

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,7 +2,11 @@ const Button = ({ filling = true, rounding, onClick, className, children }) => {
   return (
     <button
       onClick={onClick}
-      className={`${filling ? "bg-accent text-[#FFF]" : "text-accent"} ${
+      className={`${
+        filling
+          ? "bg-accent text-[#FFF] hover:bg-blue-400"
+          : "text-accent hover:text-blue-400"
+      } ${
         rounding
           ? "rounded-md px-7 py-3"
           : "px-3 py-1.5 desktop:px-6 desktop:py-3"
